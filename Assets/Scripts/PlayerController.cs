3280e7c6f43b3e6a941962e2ef104b082f56e942
Mateusz Gemza <neoneroxx@gmail.com>
2020-03-27 22:44:22 +0100

    v

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    public float jumpForce = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movment;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                transform.position += new Vector3(0, movementSpeed, 0);
+            }
+
+            Debug.Log(transform.position);
+        }
+    }
+}
