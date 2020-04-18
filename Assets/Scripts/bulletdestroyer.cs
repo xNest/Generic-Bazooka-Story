4bac720c47f3be48287d9bf1247f9de28ee5eaeb
Vanilia <kaszubakrzysiek@gmail.com>
2020-04-18 23:27:50 +0200

    cysiu done this

---
+++ Assets/Scripts/bulletdestroyer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletdestroyer : MonoBehaviour
+{
+
+    public Transform gameobject;
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameobject.name == Bullet1)
+        {
+            Destroy(gameobject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+
+  
+    public Rigidbody2D bullet;
+    private float speed = 6f;
+
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBullet",0.5f, 0.5f);
+    }
+
+
+    void SpawnBullet()
+    {
+
+
+
+        Rigidbody2D instance = Instantiate(bullet, transform);
+        instance.velocity = Vector2.right * speed;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
